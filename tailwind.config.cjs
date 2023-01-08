1b381974601a797e46b24253c9f63bb83efed08f
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-08 14:48:55 -0800

    WIP. formik. css. clarification import works wo/ postcss-import. OK

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{ts,tsx,css}'],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
