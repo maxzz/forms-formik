8719e4152e9701ea0a3b67cfa9034c66df350339
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-09 14:06:03 -0800

    WIP. app. update responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Page } from "./components/Page";
 
 export function App() {
     return (
-        <div className="min-h-screen bg-gradient-to-b from-indigo-900 to-indigo-800 grid justify-center">
+        <div className="min-h-screen bg-gradient-to-b from-indigo-900 to-indigo-500">
             <Page />
         </div>
     );
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,11 +5,13 @@ import { Form2 } from './Form2';
 export function Page() {
     const [currentForm, setCurrentForm] = useState(0);
     return (
-        <div className="w-[50ch] max-w-lg text-sm text-slate-100 grid">
+        <div className="mx-auto w-[54ch] max-w-2xl h-screen text-sm text-slate-100 grid grid-rows-[auto_minmax(0,1fr)_auto]">
+            
             <div className="py-4 flex items-center justify-center space-x-2">
                 <button className="px-4 py-3 w-max border-slate-400 border rounded" onClick={()=> setCurrentForm(0)}>Simple form</button>
                 <button className="px-4 py-3 w-max border-slate-400 border rounded" onClick={()=> setCurrentForm(1)}>Advanced form</button>
             </div>
+            
             {currentForm === 0 && <Form1 />}
             {currentForm === 1 && <Form2 />}
         </div>
