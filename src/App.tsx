52d32cfdd8c43250fdd117e1403871889b8fb8fd
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-08 19:05:59 -0800

    WIP. app. update responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Page } from "./components/Page";
 
 export function App() {
     return (
-        <div className="min-h-screen bg-gradient-to-b from-indigo-900 to-indigo-800">
+        <div className="min-h-screen bg-gradient-to-b from-indigo-900 to-indigo-800 grid justify-center">
             <Page />
         </div>
     );
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,10 +5,10 @@ import { Form2 } from './Form2';
 export function Page() {
     const [currentForm, setCurrentForm] = useState(0);
     return (
-        <div className="text-sm">
+        <div className="w-[50ch] max-w-lg text-sm text-slate-100 grid">
             <div className="py-4 flex items-center justify-center space-x-2">
-                <button className="px-4 py-3 border-slate-400 border rounded" onClick={()=> setCurrentForm(0)}>Simple form</button>
-                <button className="px-4 py-3 border-slate-400 border rounded" onClick={()=> setCurrentForm(1)}>Advanced form</button>
+                <button className="px-4 py-3 w-max border-slate-400 border rounded" onClick={()=> setCurrentForm(0)}>Simple form</button>
+                <button className="px-4 py-3 w-max border-slate-400 border rounded" onClick={()=> setCurrentForm(1)}>Advanced form</button>
             </div>
             {currentForm === 0 && <Form1 />}
             {currentForm === 1 && <Form2 />}
