7795f794dcc4f06a3c646c11f4af127c7abd14d1
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-09 17:25:28 -0800

    WIP. Form1. add ability to reset form state

--- src/components/Form1/index.tsx
+++ src/components/Form1/index.tsx
@@ -22,6 +22,7 @@ function ErrorHint({ msg }: { msg: string | boolean | undefined; }) {
 export function Form1() {
     const {
         values, errors, touched, isSubmitting, handleBlur, handleChange, handleSubmit,
+        resetForm,
     } = useFormik<Values>({
         initialValues: {
             email: '',
@@ -89,8 +90,15 @@ export function Form1() {
             </button>
         </form>
 
-        {/* <div className="px-4 text-slate-100 whitespace-pre">
+        <div className="px-4 text-slate-100 whitespace-pre">
             {JSON.stringify({ values, errors }, null, 4)}
-        </div> */}
+        </div>
+
+        <input
+            className="ml-4 mt-4 px-3 py-2 border-slate-400 hover:bg-indigo-500 border rounded active:scale-[.97]"
+            type="button"
+            value="Reset"
+            onClick={() => resetForm()}
+        />
     </>);
 }
