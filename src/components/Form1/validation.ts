c6190c4e1e92a8bf85584b576ed482d20c31a3c4
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-11 14:25:03 -0800

    WIP. Form1. build common validation as blocks

--- src/components/Form1/validation.ts
+++ src/components/Form1/validation.ts
@@ -1,18 +1,5 @@
-import { number, object, ref, string } from "yup";
-
-const ageValidation = number().positive().integer().required("Required");
-const emailValidation = string().email("Please enter a valid email").required("Required");
-
-const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/; // min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
-
-const passwordValidation = string()
-    .min(5)
-    .matches(passwordRules, { message: "Please create a stronger password" })
-    .required("Required");
-
-const passwordConfirmValidation = string()
-    .oneOf([ref("password"), null], "Passwords must match")
-    .required("Required");
+import { object } from "yup";
+import { emailValidation, ageValidation, passwordValidation, passwordConfirmValidation } from "../../schemas";
 
 export const basicSchema = object().shape({
     email: emailValidation,
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,1 +1,15 @@
-import * as yup from "yup";
+import { number, object, ref, string } from "yup";
+
+export const ageValidation = number().positive().integer().required("Required");
+export const emailValidation = string().email("Please enter a valid email").required("Required");
+
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/; // min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
+
+export const passwordValidation = string()
+    .min(5)
+    .matches(passwordRules, { message: "Please create a stronger password" })
+    .required("Required");
+
+export const passwordConfirmValidation = string()
+    .oneOf([ref("password"), null], "Passwords must match")
+    .required("Required");
