72c824ca2cb252628270317dd91b8e3ef376f66b
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-10 17:34:21 -0800

    WIP. app. add plugin for the build visualization report

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+
+        visualizer({
+            filename: 'visualization.html',
+            template: 'sunburst', // sunburst - d3 style (good as default as well); treemap - table (default); network - graph (slow to open).
+            gzipSize: true,
+            brotliSize: true,
+        }),
     ],
     server: {
         port: 3000,
