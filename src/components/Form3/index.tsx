4c100c6a47bcd0cb2095ee96d30018ed8e6ef8f6
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-10 17:39:42 -0800

    WIP. Form3. add reset and values report

--- src/components/Form3/index.tsx
+++ src/components/Form3/index.tsx
@@ -21,7 +21,7 @@ export function Form3() {
                     return new Promise((resolve) => setTimeout(resolve, 1000));
                 }}
             >
-                {({ values }) => (
+                {({ values, errors, resetForm }) => (<>
                     <Form className="flex flex-col space-y-4" autoComplete="off">
                         <Field
                             name='fullName'
@@ -35,7 +35,18 @@ export function Form3() {
                             label='Donation'
                         />
                     </Form>
-                )}
+
+                    <div className="px-4 text-slate-100 whitespace-pre">
+                        {JSON.stringify({ values, errors }, null, 4)}
+                    </div>
+
+                    <input
+                        className="ml-4 mt-4 px-3 py-2 border-slate-400 hover:bg-indigo-500 border rounded active:scale-[.97]"
+                        type="button"
+                        value="Reset"
+                        onClick={() => resetForm()}
+                    />
+                </>)}
 
             </Formik>
         </div>
