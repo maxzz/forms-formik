d423c5224e3d80976aa8ec669ec6a21b1e331ca0
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-10 17:36:10 -0800

    WIP. app. update frontend

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ export function App() {
         <div className="h-screen bg-gradient-to-b from-indigo-900 to-indigo-500 grid grid-cols-[max-content_1fr]">
 
             <div className="flex flex-col">
-                <header>
-                    Formik
+                <header className="text-[0.5rem] text-indigo-500">
+                    formik
                 </header>
                 <div className="flex-1">
                     <Page />
                 </div>
-                <footer>Max</footer>
+                <footer className="text-[0.5rem] text-indigo-600">max</footer>
             </div>
 
             <div className="bg-slate-900"></div>
