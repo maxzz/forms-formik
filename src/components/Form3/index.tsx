3dba75789a6825473343dea8fd957920680bb36a
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-10 19:39:36 -0800

    WIP. Form3. update the state report layout

--- src/components/Form3/index.tsx
+++ src/components/Form3/index.tsx
@@ -11,7 +11,7 @@ type Values = {
 export function Form3() {
     return (
         <div className="max-w-[54ch] mx-auto flex flex-col">
-            <span>Form3</span>
+
             <Formik<Values>
                 initialValues={{
                     fullName: '',
@@ -43,16 +43,19 @@ export function Form3() {
                         </Field>
                     </Form>
 
-                    <div className="px-4 text-slate-100 whitespace-pre">
-                        {JSON.stringify({ values, errors }, null, 4)}
-                    </div>
+                    <div className="mt-4">
+                        <div className="text-slate-100 whitespace-pre">
+                            {JSON.stringify({ values, errors }, null, 4)}
+                        </div>
 
-                    <input
-                        className="ml-4 mt-4 px-3 py-2 border-slate-400 hover:bg-indigo-500 border rounded active:scale-[.97]"
-                        type="button"
-                        value="Reset"
-                        onClick={() => resetForm()}
-                    />
+                        <input
+                            className="mt-4 px-3 py-2 border-slate-400 hover:bg-indigo-500 border rounded active:scale-[.97]"
+                            type="button"
+                            value="Reset"
+                            onClick={() => resetForm()}
+                        />
+
+                    </div>
                 </>)}
 
             </Formik>
