8ab0e28c841a945dcccfb3a26e31c8d72deff72d
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-10 19:55:54 -0800

    WIP. Form1. build validation as blocks

--- src/components/Form1/validation.ts
+++ src/components/Form1/validation.ts
@@ -1,15 +1,22 @@
 import { number, object, ref, string } from "yup";
 
+const ageValidation = number().positive().integer().required("Required");
+const emailValidation = string().email("Please enter a valid email").required("Required");
+
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/; // min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
 
+const passwordValidation = string()
+    .min(5)
+    .matches(passwordRules, { message: "Please create a stronger password" })
+    .required("Required");
+
+const passwordConfirmValidation = string()
+    .oneOf([ref("password"), null], "Passwords must match")
+    .required("Required");
+
 export const basicSchema = object().shape({
-    email: string().email("Please enter a valid email").required("Required"),
-    age: number().positive().integer().required("Required"),
-    password: string()
-        .min(5)
-        .matches(passwordRules, { message: "Please create a stronger password" })
-        .required("Required"),
-    confirmPassword: string()
-        .oneOf([ref("password"), null], "Passwords must match")
-        .required("Required"),
+    email: emailValidation,
+    age: ageValidation,
+    password: passwordValidation,
+    confirmPassword: passwordConfirmValidation,
 });
